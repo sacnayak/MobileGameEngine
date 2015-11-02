6c826f2ebb98a67621640bf05c95fe90de46e465
Sachin Nayak <sachin.nayak101@gmail.com>
2015-11-01 23:49:01 -0500

    Switched FSMSpec to basic xml

--- app/src/main/java/edu/cmu/ssnayak/mobilegameengine/SSUIMobileLabGameEngineActivity.java
+++ app/src/main/java/edu/cmu/ssnayak/mobilegameengine/SSUIMobileLabGameEngineActivity.java
@@ -9,7 +9,7 @@ public class SSUIMobileLabGameEngineActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        GameEngineBase gb = new GameEngineBase(R.raw.bells_whistles_fsmspec, this);
+        GameEngineBase gb = new GameEngineBase(R.raw.basic_fsmspec, this);
 
         setContentView(gb.finalInit());
         
